9721dec98fa9bfa9c0903a82c7233a69e186b898
Muhammad Alif Budiman <alifm2101@gmail.com>
2025-04-14 13:43:30 +0700

    fix: add environment configuration files

---
+++ src/environments/example.environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    emailService: {
+      serviceId: 'SERVICE_ID',
+      templateId: 'TEMPLATE_ID',
+      publicKey: 'PUBLIC_KEY',
+    },
+  };
+  
