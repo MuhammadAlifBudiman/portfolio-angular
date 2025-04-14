0329f14b03d86eb35ce18a0c89732e57ef2c599c
Muhammad Alif Budiman <alifm2101@gmail.com>
2025-04-14 13:23:08 +0700

    feat: implement contact form (#6)
    implement contact form with email service integration and environment variable injection
    
    Closes #6

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ButtonComponent } from '../../components/button/button.component';
+import { CommonModule } from '@angular/common';
+import { EmailService } from '../../services/email.service';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ButtonComponent, CommonModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
+  phoneNumber = '+6281295837271';
+  email = 'alifm2101@gmail.com';
+  alertMessage: string | null = null;
+  alertType: 'success' | 'error' | null = null;
+  time: string = '';
+  isLoading: boolean = false;
 
+  @ViewChild('contactForm') contactForm!: ElementRef<HTMLFormElement>;
+  constructor(private emailService: EmailService) {}
+
+  sendData = (): void => {
+    this.isLoading = true;
+
+    if (!this.contactForm?.nativeElement) return;
+
+    const form = this.contactForm.nativeElement as HTMLFormElement;
+
+    const currentTime = new Date().toLocaleString('en-US', {
+      dateStyle: 'medium',
+      timeStyle: 'short',
+      timeZone: 'UTC',
+    });
+
+    const timeInput = form.querySelector(
+      'input[name="time"]'
+    ) as HTMLInputElement;
+    if (timeInput) {
+      timeInput.value = currentTime;
+    }
+
+    this.emailService
+      .sendEmail(this.contactForm.nativeElement)
+      .then(() => {
+        this.alertMessage = 'Your message has been sent successfully!';
+        this.alertType = 'success';
+        this.contactForm.nativeElement.reset();
+      })
+      .catch(() => {
+        this.alertMessage = 'Failed to send your message. Please try again.';
+        this.alertType = 'error';
+      })
+      .finally(() => {
+        this.isLoading = false;
+      });
+  };
 }
