9bb20caf5e35a341fe451f02612b6580c140c289
Muhammad Alif Budiman <alifm2101@gmail.com>
2025-04-19 16:34:38 +0700

    feat: add dark mode toggle

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,6 +4,7 @@ import {
   ElementRef,
   ViewChild,
   AfterViewInit,
+  OnInit,
 } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
@@ -13,10 +14,15 @@ import { RouterModule } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent implements AfterViewInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
   @ViewChild('headerElement') headerRef!: ElementRef;
   isNavbarFixed = false;
   fixedNavOffsetTop = 0;
+  isDarkMode = false;
+
+  ngOnInit() {
+    this.loadThemePreference();
+  }
 
   ngAfterViewInit() {
     this.fixedNavOffsetTop = this.headerRef.nativeElement.offsetTop;
@@ -28,13 +34,42 @@ export class HeaderComponent implements AfterViewInit {
     this.isNavbarFixed = offset > this.fixedNavOffsetTop;
   }
 
-  toggleMenu() {
+  toggleMenu(): void {
     const hamburger = document.querySelector('#hamburger');
     const navMenu = document.querySelector('#nav-menu');
 
     if (hamburger && navMenu) {
       hamburger.classList.toggle('hamburger-active');
       navMenu.classList.toggle('hidden');
+    }
+  }
+
+  loadThemePreference(): void {
+    const html = document.documentElement;
+    const storedTheme = localStorage.getItem('theme');
+    const prefersDark = window.matchMedia(
+      '(prefers-color-scheme: dark)',
+    ).matches;
+
+    if (storedTheme === 'dark' || (!storedTheme && prefersDark)) {
+      html.classList.add('dark');
+      this.isDarkMode = true;
+    } else {
+      html.classList.remove('dark');
+      this.isDarkMode = false;
+    }
+  }
+
+  onToggleDarkMode(event: Event) {
+    const checked = (event.target as HTMLInputElement).checked;
+    this.isDarkMode = checked;
+
+    if (checked) {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
     }
   }
 }
